26ba658fb9e7bbe6bda76075a2593a6f4f4e0fa6
frase-git <cms@fhseidel.de>
2020-12-04 09:50:10 +0100

    optical improvement and adaptation to new core.css

--- config/metaconfig.php
+++ config/metaconfig.php
@@ -1,8 +1,5 @@
 <?php
-
 $plugin_mcf['hi_updatecheck']['autocheck']="bool";
 $plugin_mcf['hi_updatecheck']['autocheck_timeout']="string";
 $plugin_mcf['hi_updatecheck']['autocheck_notify']="enum:All updates,Only critical updates";
 $plugin_mcf['hi_updatecheck']['ignore']="string";
-
-?>
